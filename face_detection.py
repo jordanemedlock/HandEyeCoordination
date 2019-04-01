d79ff2cd395ea7bdc6d80e0f0fad699be0b6b61e
Jordan Medlock <jordan@innovo.net>
2019-03-31 19:34:54 -0700

    Added possible cv face detection and added margins to the bearing mount

--- face_detection.py
+++ face_detection.py
@@ -16,9 +16,32 @@ if needs_mock:
 			return (random(), random())
 
 else:
+	cascPath = 'cascade file path'
 	class FaceDetector():
 		def __init__(self):
-			pass
+			self.faceCascade = cv2.CascadeClassifier(cascPath)
+			
 
 		def find_face(self):
-			pass
+			cap = cv2.VideoCapture(0)
+			gray = cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)
+			faces = self.faceCascade.detectMultiScale(
+				gray,
+				scaleFactor=1.1,
+				minNeighbors=5,
+				minSize=(30,30),
+				flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+			)
+
+			print "Found {0} faces!".format(len(faces))
+
+			if faces:
+				(x, y, w, h) = faces[0]
+				x = x + w/2
+				y = y + h/2
+				height, width, channels = gray.shape
+				return x / width, (1 - y / height)
+			else:
+				return (random(), random())
+
+
