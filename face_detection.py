10f47c68ad53f6302e969b7529d538ca0e444494
Jordan Medlock <jmmedloc@amazon.com>
2019-04-04 20:44:45 -0700

    Converted to spaces

--- face_detection.py
+++ face_detection.py
@@ -2,51 +2,51 @@ from random import random
 
 needs_mock = False
 try:
-	import cv2
+    import cv2
 
 except ImportError:
-	needs_mock = True
+    needs_mock = True
 
 if needs_mock:
-	class FaceDetector():
-		def __init__(self):
-			pass
+    class FaceDetector():
+        def __init__(self):
+            pass
 
-		def find_face(self):
-			return (random(), random())
+        def find_face(self):
+            return (random(), random())
 
 else:
-	cascPath = '/usr/local/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
-	class FaceDetector():
-		def __init__(self):
-			self.faceCascade = cv2.CascadeClassifier(cascPath)
-			print('loaded face cascade')
-			self.cap = cv2.VideoCapture(0)
-			if self.cap.isOpened():
-				print('initialized capture')
-			else:
-				print('failed to initialize')
+    cascPath = '/usr/local/share/opencv4/haarcascades/haarcascade_frontalface_default.xml'
+    class FaceDetector():
+        def __init__(self):
+            self.faceCascade = cv2.CascadeClassifier(cascPath)
+            print('loaded face cascade')
+            self.cap = cv2.VideoCapture(0)
+            if self.cap.isOpened():
+                print('initialized capture')
+            else:
+                print('failed to initialize')
 
-		def find_face(self):
-			print('reading image')
-			re, image = self.cap.read()
-			if not re:
-				return random(), random()
-			gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-			faces = self.faceCascade.detectMultiScale(
-				gray,
-				scaleFactor=1.1,
-				minNeighbors=5,
-				minSize=(30,30)
-			)
+        def find_face(self):
+            print('reading image')
+            re, image = self.cap.read()
+            if not re:
+                return random(), random()
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = self.faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30,30)
+            )
 
-			print("Found {0} faces!".format(len(faces)))
+            print("Found {0} faces!".format(len(faces)))
 
-			for (x, y, w, h) in faces:
-				x = x + w/2
-				y = y + h/2
-				height, width, channels = gray.shape
-				return x / width, (1 - y / height)
-			return (random(), random())
+            for (x, y, w, h) in faces:
+                x = x + w/2
+                y = y + h/2
+                height, width, channels = gray.shape
+                return x / width, (1 - y / height)
+            return (random(), random())
 
 
--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 
 
 try:
-	from adafruit_servokit import ServoKit
-	kit = ServoKit
+    from adafruit_servokit import ServoKit
+    kit = ServoKit
 except ImportError:
-	from mock_servo import mock_kit
-	kit = mock_kit
+    from mock_servo import mock_kit
+    kit = mock_kit
 
