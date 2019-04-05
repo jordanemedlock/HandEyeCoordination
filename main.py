11f7d8c0cd9660a07da6fe5e0a52e3cfd1bff241
Jordan Medlock <jmmedloc@amazon.com>
2019-04-04 20:44:45 -0700

    Converted to spaces

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ fd = FaceDetector()
 gimbal = Gimbal()
 
 while True:
-	(x, y) = fd.find_face()
-	x = 2*x - 1
-	y = 2*y - 1
-	gimbal.move((x, y))
-	time.sleep(0.1)
+    (x, y) = fd.find_face()
+    x = 2*x - 1
+    y = 2*y - 1
+    gimbal.move((x, y))
+    time.sleep(0.1)
 
