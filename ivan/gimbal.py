7b26b5ae90f596208486ef0049b5bddd34168de0
Jordan Medlock <jordan@innovo.net>
2019-09-06 17:22:18 -0700

    Updated the servo stuff

--- ivan/gimbal.py
+++ ivan/gimbal.py
@@ -1,23 +1,17 @@
-try:
-    from adafruit_servokit import ServoKit
-    kit = ServoKit(channels=16)
-except:
-    from ivan.util.mock_servo import mock_kit
-    kit = mock_kit
 from ivan.util.range import Range
+from ivan.servo import Servo
 
 class Gimbal():
     def __init__(self):
-        self.kit = kit
-        self.x_axis = kit.servo[0]
-        self.y_axis = kit.servo[1]
-        self.x_loc = 60
-        self.y_loc = 60
-        self.x_range = Range(0.0, 120.0)
-        self.y_range = Range(0.0, 120.0)
+        self.x_axis = Servo.gpioServo(2)
+        self.y_axis = Servo.gpioServo(3)
+        self.x_loc = 0.5
+        self.y_loc = 0.5
+        self.x_range = Range(0.0, 1.0)
+        self.y_range = Range(0.0, 1.0)
 
 
-    def move(self, direction, speed=10):
+    def move(self, direction, speed=0.05):
         (dx, dy) = direction
         new_x = self.x_loc + dx * speed
         new_y = self.y_loc + dy * speed
@@ -26,8 +20,8 @@ class Gimbal():
         if new_y in self.y_range:
             self.y_loc = new_y
 
-        self.x_axis.angle = self.x_loc
-        self.y_axis.angle = self.y_loc
+        self.x_axis.turn_to(self.x_loc)
+        self.y_axis.turn_to(self.y_loc)
 
 
 
---
+++ ivan/servo.py
@@ -0,0 +1,66 @@
+
+
+
+class Servo(object):
+	def __init__(self):
+		self.location = 0.5
+
+	def turn_to(self, value):
+		self.location = value
+
+	def turn(self, delta):
+		self.turn_to(self.location + delta)
+
+	@classmethod
+	def mockServo(cls):
+		return MockServo()
+
+	@classmethod
+	def gpioServo(cls, pin):
+		try:
+			import gpiozero 
+			return GPIOServo(pin)
+		except:
+			return cls.mockServo()
+
+	@classmethod
+	def servoKitServo(cls, kit, index, actuation_range=120):
+		try:
+			from adafruit_servokit import ServoKit
+			kit = ServoKit(channels=16)
+			return ServoKitServo(kit, index, actuation_range=actuation_range)
+		except:
+			return cls.mockServo()
+
+
+class MockServo(Servo):
+	def turn_to(self, value):
+		super().turn_to(value)
+		print('Servo turned to {}'.format(value))
+
+
+class GPIOServo(Servo):
+	def __init__(self, pin):
+		super().__init__()
+		self.pin = pin
+		self.servo = gpiozero.Servo(pin)
+
+	def turn_to(self, value):
+		super().turn_to(value)
+		self.servo.value = value
+
+
+class ServoKitServo(Servo):
+	def __init__(self, kit, index, actuation_range=120):
+		super().__init__()
+		self.servo = kit.servo[index]
+		self.servo.actuation_range = actuation_range
+
+	def turn_to(self, value):
+		super().turn_to()
+		self.servo.angle = self.location * self.servo.actuation_range
+
+
+
+
+
